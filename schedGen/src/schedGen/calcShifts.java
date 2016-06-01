28341adad149b3b03e0c4f1d412a9fbb941ebe6d
Alex Blanchard <Alex_blanchard6262@yahoo.com>
2016-05-31 20:39:48 -0500

    buttons and forms

---
+++ schedGen/src/schedGen/calcShifts.java
@@ -0,0 +1,43 @@
+package schedGen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class calcShifts {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					calcShifts window = new calcShifts();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public calcShifts() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
