57ee155eb5f868729e17f2c16db788da68ea9ca9
nwsm <eknewsom@uark.edu>
2016-05-31 21:08:23 -0500

    Added rs2xml.jar resource. Added load button to editEmployees which fills table with * from database.

--- schedGen/src/schedGen/editEmployees.java
+++ schedGen/src/schedGen/editEmployees.java
@@ -1,15 +1,23 @@
 package schedGen;
 
 import java.awt.EventQueue;
+import java.sql.*;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
+
+import net.proteanit.sql.DbUtils;
+
 import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class editEmployees {
 
 	private JFrame frame;
-	private JTable table;
+	private JTable employeesTable;
 
 	/**
 	 * Launch the application.
@@ -26,24 +34,56 @@ public class editEmployees {
 			}
 		});
 	}
-
+	
 	/**
 	 * Create the application.
 	 */
+	String dbName = "sql5121832";
+	String url = "jdbc:mysql://sql5.freemysqlhosting.net:3306/";
+	String userName = "sql5121832";
+	String password = "WncM9gyuA9";
+	Connection connection;
+	
+	
+	
+	
+	
 	public editEmployees() {
-		initialize();
+		try{
+			connection=DriverManager.getConnection(url + dbName, userName, password);
+			Statement myStmt = connection.createStatement();
+			ResultSet myRs = myStmt.executeQuery("select * from employees");
+			initialize(myRs);
+		}catch (Exception exc){
+			System.out.println("no goood meester coke");
+		}
+		
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize(final ResultSet myRs) {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		table = new JTable();
-		frame.getContentPane().add(table, BorderLayout.CENTER);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 414, 157);
+		frame.getContentPane().add(scrollPane);
+		
+		employeesTable = new JTable();
+		scrollPane.setViewportView(employeesTable);
+		
+		JButton loadDataButton = new JButton("Load Data");
+		loadDataButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				employeesTable.setModel(DbUtils.resultSetToTableModel(myRs));
+				
+			}
+		});
+		loadDataButton.setBounds(90, 194, 138, 23);
+		frame.getContentPane().add(loadDataButton);
 	}
-
 }
